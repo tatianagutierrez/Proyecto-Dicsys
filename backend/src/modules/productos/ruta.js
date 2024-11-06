a9005a5978ffced46f45cc96994e0b4e521414bb
Tatiana Gutiérrez <90115998+tatianagutierrez@users.noreply.github.com>
2024-11-06 17:01:50 -0300

    le agrego atributos a producto

--- backend/src/modules/productos/ruta.js
+++ backend/src/modules/productos/ruta.js
@@ -4,7 +4,6 @@ import pool from '../../config.js'
 const router = express.Router()
 
 router.get('/', async (_, res) => {
-  // FIXME: fecha de vencimiento: 2024-12-01 => 2024-12-01T03:00:00.000Z
   try {
     const [result] = await pool.query('SELECT * FROM productos')
     res.send(result)
@@ -15,7 +14,7 @@ router.get('/', async (_, res) => {
 })
 
 router.post('/', async (req, res) => {
-  const { nombre, fecha_vencimiento: fechaVencimiento, id_categoria: idCategoria } = req.body
+  const { nombre, descripcion, fecha_vencimiento: fechaVencimiento, precio, stock, id_categoria: idCategoria } = req.body
   const body = req.body
 
   const producto = await buscarProductoByNombre(nombre)
@@ -24,30 +23,37 @@ router.post('/', async (req, res) => {
   }
 
   try {
-    await pool.query('INSERT INTO productos (nombre, fecha_vencimiento, id_categoria) VALUES (?, ?, ?)', [nombre, fechaVencimiento, idCategoria])
+    await pool.query('INSERT INTO productos (nombre, descripcion, fecha_vencimiento, precio, stock, id_categoria) VALUES (?, ?, ?, ?, ?, ?)', [nombre, descripcion, fechaVencimiento, precio, stock, idCategoria])
     res.status(201).json({
       mensaje: `Se creó la categoría ${nombre} con exito`,
       data: body
     })
   } catch (error) {
     console.log('Error al crear producto: ', error)
-    res.status(505).send('Error al listar productos')
+    res.status(505).send('Error al crear productos')
   }
 })
 
 router.patch('/:id', async (req, res) => {
   const { id } = req.params
+  const { nombre, descripcion, fecha_vencimiento: fechaVencimiento, precio, stock, id_categoria: idCategoria } = req.body
   const body = req.body
-  // TODO: falta validar si el nombre de la nueva producto existe
+  const updates = []
 
   const producto = await buscarProductoById(id)
-  console.log(producto)
   if (!producto) {
     return res.status(400).json({ mensaje: 'El producto no existe' })
   }
 
+  if (nombre) updates.push(`nombre = '${nombre}'`)
+  if (descripcion) updates.push(`descripcion = '${descripcion}'`)
+  if (fechaVencimiento) updates.push(`fecha_vencimiento = '${fechaVencimiento}'`)
+  if (precio) updates.push(`precio = '${precio}'`)
+  if (stock) updates.push(`stock = '${stock}'`)
+  if (idCategoria) updates.push(`id_categoria = '${idCategoria}'`)
+
   try {
-    await pool.query('UPDATE productos SET nombre = ?, fecha_vencimiento = ?, id_categoria = ? WHERE id = ?', [body.nombre, body.fecha_vencimiento, body.id_categoria, id])
+    await pool.query(`UPDATE productos SET ${updates.join(', ')} WHERE id = ${id}`)
     res.status(201).json({
       mensaje: 'Se actualizó el producto  con éxito',
       data: body
